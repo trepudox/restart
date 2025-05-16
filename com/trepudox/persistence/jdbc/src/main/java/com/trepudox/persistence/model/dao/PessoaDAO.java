719dace45444c5984cd61f5532c1ccfc1a210c6c
Marco Aurelio <teoaa2@gmail.com>
2025-05-16 11:28:16 -0300

    feat: ajustar PessoaDAO e incluir heranca do GenericDAO

--- com/trepudox/persistence/jdbc/src/main/java/com/trepudox/persistence/model/dao/PessoaDAO.java
+++ com/trepudox/persistence/jdbc/src/main/java/com/trepudox/persistence/model/dao/PessoaDAO.java
@@ -1,169 +1,40 @@
 package com.trepudox.persistence.model.dao;
 
-import com.trepudox.persistence.db.DBConnection;
 import com.trepudox.persistence.model.Pessoa;
 
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+public class PessoaDAO extends GenericDAO<Pessoa, Integer> {
 
-/*
-* Horroroso pra manutenção, o ideal seria ter as querys prontas,
-* fazer um loop usando varargs e Pattern Matching pra settar a
-* ordem dos argumentos, dessa forma seria possível usar cerca de
-* 3 métodos e atingir o mesmo resultado.
-* */
-public class PessoaDAO {
-
-    private PessoaDAO() {}
-
-    public static int create(String nome, LocalDate dataNascimento) {
-        String query = "INSERT INTO pessoa(nome, data_nascimento) VALUE (?, ?);";
-
-        try (Connection con = DBConnection.getConnection();
-             PreparedStatement stmt = con.prepareStatement(query)) {
-
-            stmt.setString(1, nome);
-            stmt.setString(2, dataNascimento.toString());
-
-            return stmt.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.printf("%nDeu erro papai: %d - %s%n%n", e.getErrorCode(), e.getMessage());
-            e.printStackTrace();
-        }
-
-        return 0;
+    @Override
+    public int create(Pessoa p) {
+        final String query = "INSERT INTO pessoa(nome, data_nascimento) VALUE (?, ?);";
+        return executeUpdate(query, p.getNome(), p.getDataNascimento());
     }
 
-    public static List<Pessoa> getAll() {
-        List<Pessoa> pessoas = new ArrayList<>();
+    @Override
+    public List<Pessoa> getAll() {
         final String query = "SELECT * FROM pessoa;";
-
-        try (Connection con = DBConnection.getConnection();
-             Statement stmt = con.createStatement()) {
-
-                try (ResultSet rs = stmt.executeQuery(query)) {
-                    while (rs.next()) {
-                        pessoas.add(Pessoa.builder()
-                                .id(rs.getInt("id"))
-                                .nome(rs.getString("nome"))
-                                .dataNascimento(rs.getDate("data_nascimento").toLocalDate())
-                                .build());
-                    }
-                }
-
-            } catch (SQLException e) {
-                System.out.printf("%nDeu erro papai: %d - %s%n%n", e.getErrorCode(), e.getMessage());
-                e.printStackTrace();
-            }
-
-        return pessoas;
+        return executeGenericQuery(query);
     }
 
-    public static Optional<Pessoa> getById(int id) {
+    @Override
+    public Optional<Pessoa> getById(Integer id) {
         final String query = "SELECT * FROM pessoa WHERE id = ?;";
-
-        try (Connection con = DBConnection.getConnection();
-             PreparedStatement stmt = con.prepareStatement(query)) {
-
-            stmt.setInt(1, id);
-            try (ResultSet rs = stmt.executeQuery()) {
-
-                if (rs.next()) {
-                    return Optional.of(Pessoa.builder()
-                            .id(rs.getInt("id"))
-                            .nome(rs.getString("nome"))
-                            .dataNascimento(rs.getDate("data_nascimento").toLocalDate())
-                            .build());
-                }
-
-            }
-
-        } catch (SQLException e) {
-            System.out.printf("%nDeu erro papai: %d - %s%n%n", e.getErrorCode(), e.getMessage());
-            e.printStackTrace();
-        }
-
-        return Optional.empty();
+        return executeSpecificQuery(query, id);
     }
 
-    public static int updateById(int id, String nome, LocalDate dataNascimento) {
-        String query = "UPDATE pessoa SET nome = ? AND data_nascimento ? WHERE id = ?;";
-
-        try (Connection con = DBConnection.getConnection();
-             PreparedStatement stmt = con.prepareStatement(query)) {
-
-            stmt.setString(1, nome);
-            stmt.setString(2, dataNascimento.toString());
-            stmt.setInt(3, id);
-
-            return stmt.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.printf("%nDeu erro papai: %d - %s%n%n", e.getErrorCode(), e.getMessage());
-            e.printStackTrace();
-        }
-
-        return 0;
+    @Override
+    public int update(Pessoa p) {
+        final String query = "UPDATE pessoa SET nome = ?, data_nascimento = ? WHERE id = ?;";
+        return executeUpdate(query, p.getNome(), p.getDataNascimento(), p.getId());
     }
 
-    public static int updateNomeById(int id, String nome) {
-        String query = "UPDATE pessoa SET nome = ? WHERE id = ?;";
-
-        try (Connection con = DBConnection.getConnection();
-             PreparedStatement stmt = con.prepareStatement(query)) {
-
-            stmt.setString(1, nome);
-            stmt.setInt(2, id);
-
-            return stmt.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.printf("%nDeu erro papai: %d - %s%n%n", e.getErrorCode(), e.getMessage());
-            e.printStackTrace();
-        }
-
-        return 0;
+    @Override
+    public int deleteById(Integer id) {
+        final String query = "DELETE FROM pessoa WHERE id = ?;";
+        return executeUpdate(query, id);
     }
 
-    public static int updateDataNascimentoById(int id, LocalDate dataNascimento) {
-        String query = "UPDATE pessoa SET data_nascimento = ? WHERE id = ?;";
-
-        try (Connection con = DBConnection.getConnection();
-             PreparedStatement stmt = con.prepareStatement(query)) {
-
-            stmt.setString(1, dataNascimento.toString());
-            stmt.setInt(2, id);
-
-            return stmt.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.printf("%nDeu erro papai: %d - %s%n%n", e.getErrorCode(), e.getMessage());
-            e.printStackTrace();
-        }
-
-        return 0;
-    }
-
-    public static int deleteById(int id) {
-        String query = "DELETE pessoa WHERE id = ?;";
-
-        try (Connection con = DBConnection.getConnection();
-             PreparedStatement stmt = con.prepareStatement(query)) {
-
-            stmt.setInt(1, id);
-
-            return stmt.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.printf("%nDeu erro papai: %d - %s%n%n", e.getErrorCode(), e.getMessage());
-            e.printStackTrace();
-        }
-
-        return 0;
-    }
 }
